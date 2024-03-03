436568d01af8588206ec0f894872c7ce24d2d23b
k4ik <kaikleal1234@gmail.com>
2024-03-02 21:22:01 -0300

    feat: create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <>
+            <p className='header-p'>welcome 🤓</p>
+            <h1 className='header-title'>BMI Calculator</h1>
+        </>
+    )
+}
+
+export default Header
