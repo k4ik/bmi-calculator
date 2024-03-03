1afc3d52a03c0d9457de6f06a863435f0a4144f8
k4ik <kaikleal1234@gmail.com>
2024-03-02 21:22:32 -0300

    feat: app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Calculator from "./components/Calculator";
+import Header from "./components/Header";
+import './styles/App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Calculator />
+    </>
+  )
+}
+
+export default App
